b2b529e3595196746de59a2b587f29449e16c9ce
揭思敏 <jsm0834@175game.com>
2018-05-05 10:41:51 +0800

    Hard code setup install_requires
    可以看到 flask 也是这样做：https://github.com/pallets/flask/blob/master/setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
 
 from setuptools import setup, find_packages
 
 __version__ = "0.1.0"
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-REQUIREMENT_FILE = os.path.join(BASE_DIR, 'requirements.txt')
-with open(REQUIREMENT_FILE, encoding='utf-8') as f:
-    install_requires = f.readlines()
 
 setup(
     name='coin_data',
@@ -18,5 +12,7 @@ setup(
     description='获取各种加密货币的价格数据',
     packages=find_packages(),
     platforms='any',
-    install_requires=install_requires,
+    install_requires=[
+        'bitcoinaverage==0.1.15'
+    ],
 )
