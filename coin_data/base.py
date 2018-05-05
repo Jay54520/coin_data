55a87f4f3c1e2c6b0b7bdc8362fb35cccc72f576
揭思敏 <jsm0834@175game.com>
2018-05-05 14:02:03 +0800

    Symbol 增加 pre_json_dict 方法
    转换为支持 json.dumps 的字典

--- coin_data/base.py
+++ coin_data/base.py
@@ -6,11 +6,22 @@ from typing import List
 
 class Symbol:
 
+    DATE_FORMAT = '%Y-%m-%d'
+
     def __init__(self, name: str, time: datetime.datetime, average_price: decimal.Decimal, currency: str):
         self.name = name
         self.time = time
         self.average_price = average_price
         self.currency = currency
+
+    def pre_json_dict(self):
+        """转换为支持 json.dumps 的字典"""
+        return {
+            'name': self.name,
+            'time': self.time.strftime(self.DATE_FORMAT),
+            'average_price': float(self.average_price),
+            'currency': self.currency,
+        }
 
 
 class AbstractDataSource:
